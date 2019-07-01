00e51c9554e3062a7637ec377f9d97325405f821
soumadiptya <soumadiptya@gmail.com>
2019-07-02 00:16:01 +0530

    Added Chapter 2 codes

--- Chapter 2/Chapter2_Templates.py
+++ Chapter 2/Chapter2_Templates.py
@@ -7,20 +7,35 @@ Created on Mon Jul  1 22:35:53 2019
 
 #%% Import libraries
 from flask import Flask
+from flask import render_template
 
 #%% Instantiate an app
 app = Flask(__name__)
+
+#%% Add data
+posts = [ 
+        {'author':'Soumadiptya Chakraborty',
+        'title':'Flask app with Layout',
+        'content':'No content... YET',
+        'date_posted':'July 1st 2019'
+        },
+        {'author':'Sara Jane Chakraborty',
+        'title':'My daughter',
+        'content':'No content... YET',
+        'date_posted':'July 2nd 2024'
+        }
+]
+
 
 #%% App Layout
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Home Page</h1>'
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
-
+    return render_template('about.html', title='About')
 #%% Run app
 if __name__ == '__main__':
     app.run(debug=True)
